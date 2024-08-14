ccc53f12754b52d0f0715a949b1f32b33b10c990
Najib Adan <najibadan1@gmail.com>
2024-08-14 20:39:59 +0300

    Random greetings

--- 02_greetings/greetings/greetings.go
+++ 02_greetings/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person
@@ -13,6 +14,18 @@ func Hello(name string) (string, error) {
 	}
 	// if name was received, return a value that embeds the name
 	// in a greeting message
-	message := fmt.Sprintf("Hi %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Returns a randomly selected message format by specifying a random index for the slice of formats
+	return formats[rand.Intn(len(formats))]
 }
--- 02_greetings/hello/hello.go
+++ 02_greetings/hello/hello.go
@@ -12,15 +12,18 @@ func main() {
 	// the log entry prefix and a flag to disable printing the time, source file and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
+	// looop through 3 times to see the variations of the greetings returned
+	for i := 0; i < 3; i++ {
 
-	//Requet a greeting message
-	message, err := greetings.Hello("")
+		//Requet a greeting message
+		message, err := greetings.Hello("Najib")
 
-	//if an error was returned, print it to console and exit the program
-	if err != nil {
-		log.Fatal(err)
+		//if an error was returned, print it to console and exit the program
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// if no error was returned, print the returned message to the console
+		fmt.Println(message)
 	}
-
-	// if no error was returned, print the returned message to the console
-	fmt.Println(message)
 }
