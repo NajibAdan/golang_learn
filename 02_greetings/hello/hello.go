75b6de4cc804c342f5a0e7efc095423a494899d1
Najib Adan <najibadan1@gmail.com>
2024-08-15 07:13:21 +0300

    Multipple greetings

--- 02_greetings/hello/hello.go
+++ 02_greetings/hello/hello.go
@@ -13,17 +13,19 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 	// looop through 3 times to see the variations of the greetings returned
-	for i := 0; i < 3; i++ {
+	// A slice of names.
+	names := []string{"Najib", "Adan", "Teloo"}
+	//Requet a greeting message
+	messages, err := greetings.Hellos(names)
 
-		//Requet a greeting message
-		message, err := greetings.Hello("Najib")
-
-		//if an error was returned, print it to console and exit the program
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// if no error was returned, print the returned message to the console
-		fmt.Println(message)
+	//if an error was returned, print it to console and exit the program
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	// if no error was returned, print the returned message to the console
+	for _, name := range names {
+		fmt.Println(messages[name])
+	}
+	fmt.Println(messages)
 }
